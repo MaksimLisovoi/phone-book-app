e63aa9362d2d35c8069f13394f3d8332b8613698
MaksimLisovoi <lisovoi123@gmail.com>
2023-03-22 12:55:26 +0200

    ++

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import PropTypes from 'prop-types';
 
 import toast from 'react-hot-toast';
--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 
 import { PersistGate } from 'redux-persist/integration/react';
-import { createTheme } from '@mui/material';
 
 // const darkTheme = createTheme({
 //   palette: {
